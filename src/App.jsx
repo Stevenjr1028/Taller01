6869eecfa099a531749e8113e360ae146c44a11c
Gerson Steven <98818664+Stevenjr1028@users.noreply.github.com>
2023-03-01 13:27:39 -0500

    Punto8

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import SameNumberCounter from './components/Punto4'
 import NumberList from './components/Punto5'
 import ImparesMenores from './components/Punto6'
 import AumentoSalario from './components/Punto7'
+import DivisionArreglo from './components/Punto8'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +21,7 @@ function App() {
       <NumberList/>
       <ImparesMenores/>
       <AumentoSalario/>
+      <DivisionArreglo/>
     </div>
     
   )
---
+++ src/components/Punto8.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const DivisionArreglo = () => {
+  const [numeros, setNumeros] = useState(
+    Array.from({ length: 10 }, () => Math.floor(Math.random() * 100))
+  );
+  const [indice, setIndice] = useState("");
+  const [resultados, setResultados] = useState([]);
+
+  const handleIndiceChange = (event) => {
+    setIndice(parseInt(event.target.value));
+  };
+
+  const dividirArreglo = () => {
+    if (isNaN(indice) || indice < 1 || indice > 10) {
+      alert("Debe ingresar un índice válido.");
+    } else {
+      let divisor = numeros[indice - 1];
+      let resultados = numeros.map((numero) => numero / divisor);
+      setResultados(resultados);
+    }
+  };
+
+  return (
+    <div className="container mx-auto">
+      <h2 className="text-2xl font-bold mb-4">Arreglo de números aleatorios:</h2>
+      <p className="mb-4">{numeros.join(", ")}</p>
+      <label htmlFor="indice" className="block mb-2">
+        Ingrese un índice entre 1 y 10:
+      </label>
+      <input
+        id="indice"
+        type="number"
+        value={indice}
+        onChange={handleIndiceChange}
+        className="block border border-gray-400 py-2 px-4 mb-4 rounded-md w-full"
+      />
+      <button
+        onClick={dividirArreglo}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Dividir arreglo
+      </button>
+      {resultados.length > 0 ? (
+        <div className="mt-4">
+          <h2 className="text-2xl font-bold mb-4">Arreglo resultante:</h2>
+          <p>{resultados.join(", ")}</p>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default DivisionArreglo;
